25f5449fb36bd16c3b98893d7d63fdf87a353bdf
unwabuisi <unwabuisi@utexas.edu>
2018-12-31 20:19:37 -0600

    added twitter keys, and connected keys.js to liri.js to pull twitter keys into the main app.

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.twitterKeys = {
+  consumer_key: '1t4uDL06twho1pUJoOTrxQ',
+  consumer_secret: 'HCEjhFD3F8VXsztI5TJ6vj23UK0EqKMCvCbS8iV7g7I',
+  access_token_key: '70069921-fIYdAEZ3bSQZYsm9OPYjmj4PNWEVy7VEfC8FdpQlo',
+  access_token_secret: '73iyutpqrgi8qMhib2I3u2OQmgHkcpZb1hqbfdQz844',
+};
+console.log('Twitter loaded');
---
+++ liri.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+var twitterKeys = require("./keys.js");
+
+var action = process.argv[2];
+
+
+
+
