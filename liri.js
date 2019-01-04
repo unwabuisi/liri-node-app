88a6d92ff202bc9b76f8ef7fc18ce436ead8476c
unwabuisi <unwabuisi@utexas.edu>
2019-01-03 21:33:34 -0600

    omdb api functionality added with correct output

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-// var request = require("request");
+var request = require("request");
 
 var action = process.argv[2];
 
@@ -78,6 +78,50 @@ function spotifySearch() {
 }
 
 function movieSearch() {
+
+    var movie = 'Mr. Nobody';
+    var imdbID = 'tt0485947';
+
+    if (process.argv[3]) {
+        movie = process.argv[3];
+        // handles movie names with more than one word
+        if (process.argv.length > 3) {
+            for (var i = 4; i < process.argv.length; i++) {
+                movie += ' ' + process.argv[i];
+            }
+        }
+    }
+
+    // initial search
+    request('http://www.omdbapi.com/?apikey=trilogy&s='+ movie + '&r=json&type=movie', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieData = JSON.parse(body).Search[0];
+            imdbID = movieData.imdbID;
+        }
+
+        // Full search
+        request('http://www.omdbapi.com/?apikey=trilogy&i=' + imdbID +'', function(error2, response2, body2) {
+            var fullMovieData = JSON.parse(response2.body);
+            var actors = JSON.parse(response2.body).Actors;
+            var title = JSON.parse(response2.body).Title;
+            var releaseYear = JSON.parse(response2.body).Released;
+            var imdbRating = JSON.parse(response2.body).imdbRating;
+            var language = JSON.parse(response2.body).Language;
+            var countries = JSON.parse(response2.body).Country;
+            var plot = JSON.parse(response2.body).Plot;
+            var rottenTRating = JSON.parse(response2.body).Ratings[1].Value;
+
+
+            console.log('\n\n**** LIRI ****\n');
+            console.log('Movie Title: ' + title + '\nActors: ' + actors + '\nDate Released: ' + releaseYear);
+            console.log('IMDB Rating: '+imdbRating+ '\nLanguage: '+language+'\nCountries: '+countries+'\nPlot: '+plot);
+            console.log('Rotten Tomato Rating: '+rottenTRating+'');
+            console.log('\n**** LIRI ****\n\n');
+        });
+    });
+
+
+
 
 }
 
