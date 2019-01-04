56c6301554285a627a6357c1482bc00b182f7a59
unwabuisi <unwabuisi@utexas.edu>
2019-01-03 20:14:44 -0600

    spotify function displaying correct outputs

--- liri.js
+++ liri.js
@@ -46,12 +46,26 @@ function spotifySearch() {
 
     spotify.search({
         type: 'track',
-        query: song
+        query: song,
+        limit: 1
     }, function(err,data) {
         if (err) {
             return console.log('Error occured: ' + err);
         }
-        console.log(JSON.stringify(data, null, ' '));
+        // var songData = JSON.stringify(data, null, ' ');
+        // console.log(songData);
+
+        var artistName = data.tracks.items[0].album.artists[0].name;
+        var albumName = data.tracks.items[0].album.name;
+        var previewLink = data.tracks.items[0].album.artists[0].external_urls.spotify;
+        var trackName = data.tracks.items[0].name;
+
+        console.log('\n\n**** LIRI ****\n');
+        console.log('Artist: ' + artistName);
+        console.log('Song: ' + trackName);
+        console.log('Preview Link: ' + previewLink);
+        console.log('Album: ' + albumName);
+        console.log('\n**** LIRI ****\n\n');
     });
 
 
@@ -89,4 +103,8 @@ switch (action) {
     case 'do-what-it-says':
         doWhatItSays();
         break;
+    default:
+        console.log('\n\n**** LIRI ****\nERROR: You did not enter a recognized command!\n');
+        console.log('Please enter one of the following commands (ex: node liri.js <command>):');
+        console.log('\n> my-tweets\n> spotify-this-song <song name here>\n> movie-this <movie name here>\n> do-what-it-says\n\n');
 }
